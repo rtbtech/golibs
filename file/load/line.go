9320ee1dda44f78804cb143a44326042206792db
Oleg Romanenko <oleg@romanenko.ro>
2021-10-13 15:07:04 +0300

    Add buffer size to Load by line

--- file/load/line.go
+++ file/load/line.go
@@ -20,3 +20,23 @@ func ByLine(path string, parse func(line string)) error {
 
 	return scanner.Err()
 }
+
+func ByLineMax(path string, maxSize int, parse func(line string)) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+    buf := make([]byte, maxSize)
+    scanner.Buffer(buf, cap(buf))
+
+	for scanner.Scan() {
+		parse(scanner.Text())
+	}
+
+	return scanner.Err()
+}
